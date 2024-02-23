623050b676ebc5effb9a1a8e5cfbd6ba816aded5
Andrés Peláez <andrespelaez24@gmail.com>
2024-02-23 17:49:48 -0500

    Added new Soil sensor

--- SoilMoistureSensor.h
+++ SoilMoistureSensor.h
@@ -8,7 +8,7 @@ class SoilMoistureSensor : public PollingComponent, public sensor::Sensor {
 //define the receiver pin
 int output_value ;
 //integer to store the output value of soil moiture sensor
-const int analogOutPin = 1; 
+//const int analogOutPin = 1; 
 //set analog output pin to pin 1
 int threshold = 800;
 //set the threshold value = 800
@@ -23,22 +23,24 @@ int current_value;
     }
 
     void update() override {
-      Serial.println("-- update --");
-      float level = (float)analogRead(A0)/10;
+      // Serial.println("-- update --");
 
-      Serial.println(analogRead(A0));
+      // Serial.println(analogRead(A0));
 
       output_value= analogRead(A0);
-        //read the value of soil moisture sensor and store it in output_value 
-        current_value = output_value;
-        //set current_value equal to output_value
-        output_value= constrain(output_value,10,1023); 
-        //set the contrain on output_value. Minimum = 10 Maximum = 1023 
-        output_value= map(output_value,10,1023,150,0); 
+      
+      Serial.println("-- analogRead --");
+      Serial.println(output_value);
+      //read the value of soil moisture sensor and store it in output_value 
+      current_value = output_value;
+      //set current_value equal to output_value
+      output_value= constrain(output_value,10,1023); 
+      //set the contrain on output_value. Minimum = 10 Maximum = 1023 
+      output_value= map(output_value,300,700,120,0); 
         
       Serial.println("-- output_value --");
       Serial.println(output_value);
-      Serial.println(level);
+
       publish_state(output_value);
     }
 };
